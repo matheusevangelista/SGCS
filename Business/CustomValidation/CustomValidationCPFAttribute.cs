6b581702d74ce3ba79d6ae7a641bbcf1ab6dadaf
matheusevangelista <matheus_0604@hotmail.com>
2015-11-28 19:59:56 -0200

    custom validation

---
+++ Business/CustomValidation/CustomValidationCPFAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Business.CustomValidation
+{
+    public class CustomValidationCPFAttribute : ValidationAttribute, IClientValidatable
+    {
+        public CustomValidationCPFAttribute() { }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+                return false;
+
+            return Utils.Validations.ValidaCPF(value.ToString());
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            yield return new ModelClientValidationRule
+            {
+                ErrorMessage = this.FormatErrorMessage("CPF Inválido!"),
+                ValidationType = "customvalidationcpf"
+            };
+        }
+    }
+}
